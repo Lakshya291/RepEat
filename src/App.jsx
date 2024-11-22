75ef5c1f83f9038c33981b6c900a01fecf90414c
Lakshya Gupta <72075811+Lakshya291@users.noreply.github.com>
2024-11-23 02:00:32 +0530

    Updated App.jsx to handle the scrolling of formulate button

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,11 @@ function App() {
 			return;
 		}
 		let newWorkout = generateWorkout({ poison, muscles, goal });
-		// console.log(newWorkout);
 		setWorkout(newWorkout);
-		window.location.href = "#workout";
+		console.log("Formulated");
+		setTimeout(() => {
+			window.location.href = "#workout";
+		}, 100);
 	}
 	return (
 		<main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
